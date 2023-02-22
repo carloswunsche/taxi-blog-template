3f80241a2edba47db3e8db4b5f5894b1a647ea60
Carlos Wunsche <wunschecarlos@gmail.com>
2023-02-22 16:24:03 -0500

    fixing little stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,66 +1,63 @@
-const Link = require("./src/_includes/components/Link.js");
-const Icon = require("./src/_includes/components/Icon.js");
-const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
-const htmlmin = require("html-minifier");
-const CleanCSS = require("clean-css");
-const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
-const markdownIt = require("markdown-it");
-const { DateTime } = require("luxon");
+const Link = require('./src/_includes/components/Link.js');
+const Icon = require('./src/_includes/components/Icon.js');
+const inclusiveLangPlugin = require('@11ty/eleventy-plugin-inclusive-language');
+const CleanCSS = require('clean-css');
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+const markdownIt = require('markdown-it');
+const { DateTime } = require('luxon');
+const htmlmin = require('html-minifier');
 // Optional, not used
-const htmlBeautify = require("html-beautify");
-const htmlPrettify = require("html-prettify");
+// const htmlBeautify = require('html-beautify');
+// const htmlPrettify = require('html-prettify');
 
 module.exports = function (eleventyConfig) {
   // Quiet mode
   eleventyConfig.setQuietMode(true);
 
   // Passthrough some directories
-  eleventyConfig.addPassthroughCopy("./src/images");
-  eleventyConfig.addPassthroughCopy("./src/js");
-  eleventyConfig.addPassthroughCopy({ "src/_includes/css": "css" });
+  eleventyConfig.addPassthroughCopy('./src/images');
+  eleventyConfig.addPassthroughCopy('./src/js');
+  eleventyConfig.addPassthroughCopy({ 'src/_includes/css': 'css' });
   // eleventyConfig.addWatchTarget("./src/css");
 
   // Browsersync config (fix css not updating)
   eleventyConfig.setBrowserSyncConfig({ watch: true });
 
   // Shortcodes
-  eleventyConfig.addShortcode("Link", Link);
-  eleventyConfig.addShortcode("Icon", Icon);
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addShortcode('Link', Link);
+  eleventyConfig.addShortcode('Icon', Icon);
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
   // Plugins
   eleventyConfig.addPlugin(inclusiveLangPlugin);
   eleventyConfig.addPlugin(lazyImagesPlugin, {
-    imgSelector: ".lazy", // custom image selector
+    imgSelector: '.lazy', // custom image selector
   });
 
   // Enable excerpts (for the top post in hero section)
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
-    // Optional, default is "---"
-    // excerpt_separator: '<!-- excerpt -->',
-    // Optional, default is "page.excerpt"
-    excerpt_alias: "excerpt",
+    excerpt_alias: 'excerpt',
   });
 
   // Filters
   // Minify CSS
-  eleventyConfig.addFilter("cssmin", function (code) {
+  eleventyConfig.addFilter('cssmin', function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
   // Format datetime
-  eleventyConfig.addFilter("postDate", (dateObj) => {
+  eleventyConfig.addFilter('postDate', dateObj => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
   // Markdown-it
   const md = new markdownIt({ html: true });
-  eleventyConfig.addFilter("markdown", (content) => {
+  eleventyConfig.addFilter('markdown', content => {
     return md.render(content);
   });
 
-  // Transforms
+  // Transforms (not used)
   // Beautify HTML
   // eleventyConfig.addTransform("htmlBeautify", function(content, outputPath) {
   //     if( this.inputPath && this.outputPath.endsWith(".html") ) {
@@ -70,7 +67,7 @@ module.exports = function (eleventyConfig) {
   //     return content;
   // });
 
-  // Pretify HTML
+  // Pretify HTML (not used)
   // eleventyConfig.addTransform("htmlPrettify", function(content, outputPath) {
   //   if( this.inputPath && this.outputPath.endsWith(".html") ) {
   //     let pretty = htmlPrettify(content);
@@ -80,28 +77,28 @@ module.exports = function (eleventyConfig) {
   // });
 
   // Minify HTML
-  // eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-  //   if( this.inputPath && this.outputPath.endsWith(".html") ) {
-  //     let minified = htmlmin.minify(content, {
-  //       useShortDoctype: true,
-  //       removeComments: true,
-  //       collapseWhitespace: true
-  //     });
-  //     return minified;
-  //   }
-  //   return content;
-  // });
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (this.inputPath && this.outputPath.endsWith('.html')) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+    return content;
+  });
 
   // Return
   return {
     dir: {
-      input: "src",
-      includes: "_includes",
-      output: "_site",
+      input: 'src',
+      includes: '_includes',
+      output: '_site',
     },
-    templateFormats: ["md", "njk", "html"],
-    markdownTemplateEngine: "md",
-    htmlTemplateEngine: "html",
-    dataTemplateEngine: "njk",
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'md',
+    htmlTemplateEngine: 'html',
+    dataTemplateEngine: 'njk',
   };
 };
